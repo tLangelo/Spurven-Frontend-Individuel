2408fdd361635af4f4b1bf0260d6e92f73fe17db
tLangelo <theislangelo@gmail.com>
2022-12-16 14:04:20 +0100

    changed something super secret

--- settings.js
+++ settings.js
@@ -1,3 +1,2 @@
-export const API_URL = "https://spurven-backend.azurewebsites.net/"
+export const API_URL = "https://spurven-tl.azurewebsites.net/";
 //export const API_URL = "http://127.0.0.1:8080/"
-    
